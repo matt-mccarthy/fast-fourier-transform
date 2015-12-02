e68cf0b7726074527e50425a7f5324d09e29be0b
Matt McCarthy <matt.mccarthy@protonmail.ch>
2015-12-02 18:23:08 -0500

    Added shell cpp

--- fast-fourier/include/src/fast-fourier.cpp
+++ fast-fourier/include/src/fast-fourier.cpp
@@ -1,1 +1,14 @@
 #include "../fast-fourier.h"
+
+void spawned(unsigned l, unsigned n, cuFloatComplex* x_l,
+						cuFloatComplex* y_l)
+{
+
+}
+
+cuFloatComplex* fast_fourier::fast_fourier_transform(cuFloatComplex* x,
+														unsigned n, unsigned p)
+{
+	cuFloatComplex* y(new cuFloatComplex[n]);
+
+}
