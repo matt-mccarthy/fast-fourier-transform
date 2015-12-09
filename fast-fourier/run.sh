71fbe3cc37f1f6830023de10978c28937776f9de
Matt McCarthy <matt.mccarthy@protonmail.ch>
2015-12-08 22:16:23 -0500

    Added comments to shell script

--- fast-fourier/run.sh
+++ fast-fourier/run.sh
@@ -1,22 +1,25 @@
 out_dir="results"
 trials=1000
+# Must be powers of two.
 arr_size=(4096 8192 16384 32768 65536 131072 262144 524288 1048576)
+# Must also be powers of two. I pass this twice since I use num_blks=num_thds.
 num_thds=(2 4 8 16 32)
-
+# The names of the executables
 seq_file="./sequential-fft"
 par_file="./parallel-fft"
-
+# Clean the result directory
 rm -rf $out_dir
 mkdir $out_dir
-
+# Run sequential
 for size in "${arr_size[@]}"
 do
 	out=$($seq_file $size $trials)
 	echo -e $size'\t'$out >> $out_dir/1.txt
 done
-
+# Run parallel
 for threads in "${num_thds[@]}"
 do
+	# Total number of threads = num_blk * num_thd
 	exprans=`expr $threads \\* $threads`
 	for size in "${arr_size[@]}"
 	do
