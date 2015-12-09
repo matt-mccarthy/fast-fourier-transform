8048a5ac12cc467435afca15302afabcbfdf79a9
Matt McCarthy <matt.mccarthy@protonmail.ch>
2015-12-08 22:12:42 -0500

    Finalized shell script. Gonna run this thing overnight

--- fast-fourier/run.sh
+++ fast-fourier/run.sh
@@ -1,25 +1,27 @@
 out_dir="results"
-trials=1000
-arr_size=(4096 8192 16384 32768 65536 131072)
-num_thds=("4 4" "8 8" "16 16" "32 32")
+trials=10
+#arr_size=(4096 8192 16384 32768 65536 131072)
+arr_size=(1024 2048)
+num_thds=(2 4 8 16 32)
 
-seq_file="sequential-fft"
-par_file="parallel-fft"
+seq_file="./sequential-fft"
+par_file="./parallel-fft"
 
 rm -rf $out_dir
 mkdir $out_dir
 
 for size in "${arr_size[@]}"
 do
-	out=$($seq_file $threads $size $trials)
+	out=$($seq_file $size $trials)
 	echo -e $size'\t'$out >> $out_dir/1.txt
 done
 
 for threads in "${num_thds[@]}"
 do
+	exprans=`expr $threads \\* $threads`
 	for size in "${arr_size[@]}"
 	do
-		out=$($par_file $threads $size $trials)
-		echo -e $size'\t'$out >> $out_dir/$threads.txt
+		out=$($par_file $size $trials $threads $threads)
+		echo -e $size'\t'$out >> $out_dir/$exprans.txt
 	done
 done
