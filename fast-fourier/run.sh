794232a1d20af167311682d8d3840aaca2e98be2
Matt McCarthy <matt.mccarthy@protonmail.ch>
2015-12-10 09:31:29 -0500

    Got results

--- fast-fourier/run.sh
+++ fast-fourier/run.sh
@@ -1,8 +1,8 @@
 out_dir="results"
 trials=1000
 # Must be powers of two.
-# I go from 32K (4096 float2's) to 8M (1048576 float2's)
-arr_size=(4096 8192 16384 32768 65536 131072 262144 524288 1048576)
+# I go from 32K (4096 float2's) to 2M (262144 float2's)
+arr_size=(4096 8192 16384 32768 65536 131072 262144)
 # Must also be powers of two. I pass this twice since I use num_blks=num_thds.
 num_thds=(2 4 8 16 32)
 # The names of the executables
@@ -14,6 +14,7 @@ mkdir $out_dir
 # Run sequential
 for size in "${arr_size[@]}"
 do
+	echo 1 $size
 	out=$($seq_file $size $trials)
 	echo -e $size'\t'$out >> $out_dir/1.txt
 done
@@ -24,6 +25,7 @@ do
 	exprans=`expr $threads \\* $threads`
 	for size in "${arr_size[@]}"
 	do
+		echo $exprans $size
 		out=$($par_file $size $trials $threads $threads)
 		echo -e $size'\t'$out >> $out_dir/$exprans.txt
 	done
